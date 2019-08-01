9026ca1704002c543355080d645be57f803e245d
user1 <user1@163.com>
2019-08-01 10:56:06 +0800

    卫菲雪

--- hospital_manage_system/settings.py
+++ hospital_manage_system/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'medicine_house',       # 药房（药物的去留）
     'pay_money_place',      # 缴费处（提供缴费账单，以及发放药品）
     'doctor',            # 医生（查看病人病历，提供药物清单，以及是否住院治疗）
-    'login_register'    # 登录注册以及密码修改
+    'login_register',    # 登录注册以及密码修改
+    # 'administrator.templatetags.filters'
 
 
 ]
@@ -74,6 +75,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        # 'libraries':{'filters':'administrator.templatetags.filters'}
     },
 ]
 
